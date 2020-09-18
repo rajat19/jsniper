05d85f1c1e614579c9874fd7d69ac16670f4a8c6
evilj0ker <rajatsri94@gmail.com>
2020-09-18 19:57:02 +0530

    restructure files and add support to extend templates

---
+++ utils/options.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export interface CliOptions {
+    projectName: string
+    projectDescription: string
+    templateName: string
+    templatePath: string
+    targetPath: string
+}
+
+const CHOICES = fs.readdirSync(path.join(__dirname, '../templates'));
+
+export const QUESTIONS = [
+{
+    name: 'template',
+    type: 'list',
+    message: 'What project template would you like to generate?',
+    choices: CHOICES
+},
+{
+    name: 'name',
+    type: 'input',
+    message: 'Project name:'
+}, 
+{
+    name: 'description',
+    type: 'input',
+    message: 'Project Description'
+}];
--- utils/template.ts
+++ utils/template.ts
@@ -1,6 +1,7 @@
 import * as ejs from 'ejs';
 export interface TemplateData {
     projectName: string
+    projectDescription: string
 }
 export function render(content: string, data: TemplateData) {
     return ejs.render(content, data);
