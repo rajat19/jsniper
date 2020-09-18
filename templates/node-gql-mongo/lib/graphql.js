be8ab1eb20245152fd4d747496b41ccf084cdd3c
evilj0ker <rajatsri94@gmail.com>
2020-09-18 20:39:42 +0530

    change templates and add data in them

---
+++ templates/node-gql-mongo/lib/graphql.js
@@ -0,0 +1,35 @@
+const makeExecutableSchema = require('graphql-tools').makeExecutableSchema;
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+
+const modules = [
+	'user',
+];
+
+const getTypeDefs = (modules) => {
+	const paths = ['base.graphql'];
+	modules.forEach(module => paths.push(path.join(module, 'schema.graphql')));
+	const schemas = paths.map(p => fs.readFileSync(path.join(__dirname, p), {encoding: 'utf-8', flag: 'r'}));
+	return schemas.join('\n');
+};
+
+const getResolvers = (modules) => {
+	const resolvers = {
+		Query: {},
+		Mutation: {},
+	};
+	modules.forEach((module) => {
+		const resolver = require(path.join(__dirname, module, 'resolver.js'));
+		if (resolver.Query) resolvers.Query = Object.assign(resolvers.Query, resolver.Query);
+		if (resolver.Mutation) resolvers.Mutation = Object.assign(resolvers.Mutation, resolver.Mutation);
+		const capitalCase = _.capitalize(module);
+		if (resolver[capitalCase]) resolvers[capitalCase] = resolver[capitalCase];
+	});
+	return resolvers;
+};
+
+const resolvers = getResolvers(modules);
+const typeDefs = getTypeDefs(modules);
+
+module.exports = makeExecutableSchema({typeDefs, resolvers});
